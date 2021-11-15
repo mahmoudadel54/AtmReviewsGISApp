36393bbacc37eff41dfbb7fc6792297f7decd8f9
mahmoudadel54 <mahmoodmadel@gmail.com>
2021-11-15 10:17:52 +0200

    handle routing and reset routing

--- client/src/components/MainPage/mapComponent.js
+++ client/src/components/MainPage/mapComponent.js
@@ -1,15 +1,26 @@
+import "leaflet/dist/leaflet.css";
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import L from "leaflet";
 import axiosInstance from "../../utils/axiosInstance";
 import atmIconImg from "../../assets/icons/atm2.png";
-import 'leaflet.fullscreen/Control.FullScreen';
+import L, { map } from "leaflet";
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.min.js'
+//leaflet controls
 import { OpenStreetMapProvider,GeoSearchControl } from 'leaflet-geosearch';
+import 'leaflet.fullscreen/Control.FullScreen';
+import 'leaflet-navbar';
 import 'leaflet-feature-legend/src/feature-legend';
+import 'leaflet-mouse-position';
+import { closest } from 'leaflet-geometryutil/src/leaflet.geometryutil'
+
 import 'leaflet-geosearch/dist/geosearch.css'
 import 'leaflet-feature-legend/src/feature-legend.css';
 import 'leaflet.fullscreen/Control.FullScreen.css';
-import "leaflet/dist/leaflet.css";
+import 'leaflet-navbar/Leaflet.NavBar.css';
+import 'leaflet-mouse-position/src/L.Control.MousePosition.css'
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
+import locationIcon from "../../assets/icons/location.png";
+import markerIcon from "../../assets/icons/marker.png";
 let atmIcon = new L.Icon({
   iconUrl: atmIconImg,
   iconSize: [20, 20],
@@ -18,6 +29,23 @@ let atmIcon = new L.Icon({
 });
 function MapComponent(props) {
   const [atmData, setAtmData] = useState(null);
+  const [currentLocation, setCurrentLocationMarker] = useState(null);
+  const [routingControl, setRoutingControl] = useState(
+    L.Routing.control({
+      waypoints: [],
+      routeWhileDragging: false,
+      collapsible:true,
+      plan: new L.Routing.plan([],{
+        createMarker: function () {
+          return null;
+        },
+      }),
+      show:false,
+      // collapsible:false
+    })
+  )
+
+  let map = props.mapInstance;
   const fetchAtmData = async () => {
     try {
       let res = await axiosInstance.get("/atm", {
@@ -85,10 +113,48 @@ const legend = L.control.featureLegend({
 legend.addTo(map);
 L.control.scale().addTo(map);
 
-    map.on('click',()=>{
+L.control.navbar().addTo(map);
+L.control.mousePosition({
+  emptystring:"",
+  prefix:"Lat - Lng"
+}).addTo(map);
+map.addControl(routingControl)
+map.on('click',()=>{
             props.setShowReviewList({ bool: false, atmId: "",latlng:"" });
             props.setReviewData([]);
           
+          })
+          map.on('locationfound',(e)=>{
+            debugger
+            console.log(e);
+            console.log({closest});
+            let closestATM = (closest(map,Object.values(map._layers).filter(i=>i.type==="atm").map(i=>i._latlng),e.latlng,true))
+            console.log(closestATM);
+            routingControl.setWaypoints([
+              L.latLng(closestATM.lat, closestATM.lng),
+              e.latlng
+            ]);
+            window.map = map
+            let currentLoc = L.marker(e.latlng,{
+              icon:new L.icon({
+                iconUrl: markerIcon,
+                iconSize: [30, 30],
+                iconAnchor: [15, 15],
+                popupAnchor: [0, 0],
+              }), type:"myLocation"
+            });
+            map.addLayer(currentLoc);
+            setCurrentLocationMarker(currentLoc)
+            console.log(routingControl);
+            // routingControl.on('routesfound',(e)=>{
+            //   console.log(e);
+            // })
+          })
+          routingControl.on('routeselected',(e)=>{
+            map.flyToBounds(routingControl._line);
+            console.log({routingControl});
+            console.log(e);
+      
           })
     setMapInstance(map);
     return () => {
@@ -102,7 +168,7 @@ L.control.scale().addTo(map);
     return () => {
     };
   }, [atmData]);
-  const addLayerToMap = (data, map) => {
+  const addLayerToMap = (data) => {
     let atmLayer = L.geoJSON(
       {
         type: "FeatureCollection",
@@ -141,6 +207,11 @@ L.control.scale().addTo(map);
           });
           return marker;
         },
+        onEachFeature:function (feature, layer) {
+          layer.type="atm"
+          
+        }
+
       }
     );
     atmLayer.on("add", (e) => {
@@ -148,9 +219,36 @@ L.control.scale().addTo(map);
       map.fitBounds(atmLayer.getBounds());
     });
     map.addLayer(atmLayer);
-    console.log({map});
+    console.log({map},{atmLayer});
   };
-  return <div id="mapID"></div>;
+  const handleGoToNearsetATM =()=>{
+
+    map.locate({
+      enableHighAccuracy:true,
+      setView:true,
+      // watch:true
+    })
+   
+  }
+  const handleRemoveRoute=()=>{
+    routingControl.setWaypoints([]);
+    console.log(map._layers);
+    if(currentLocation){
+    map.removeLayer(currentLocation);
+    setCurrentLocationMarker(null)
+    // map.stopLocate()
+    }
+  }
+  return <div id="mapID">
+    <div className="leaflet-top leaflet-left location">
+    <div className="leaflet-bar leaflet-control location" onClick={handleGoToNearsetATM}>
+      <img className="location-control" title="Go to nearest atm" src={locationIcon} alt="my Location" width="10" />
+    </div>
+    <div className="leaflet-bar leaflet-control location" onClick={handleRemoveRoute}>
+  <button className="btn btn-danger">Reset Route</button>
+    </div>
+    </div>
+  </div>;
 }
 const mapStateToProps = ({ user }) => {
   return {
--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/js/all.js'
 import '@fortawesome/fontawesome-free/css/all.css'
+import "leaflet/dist/leaflet.css";
 import './index.css';
 
 ReactDOM.render(
